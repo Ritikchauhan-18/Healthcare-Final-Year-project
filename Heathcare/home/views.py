2a382f61b4960f9b1aa5f03b27091a94a99e6584
Ritik Chauhan <chauhansahab0852@gmail.com>
2024-05-26 13:30:09 +0530

    All files are added

--- Heathcare/home/views.py
+++ Heathcare/home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        # name = request.POST.get('name')
+        return render(request, 'home/result.html')
     return render(request, 'home/index.html')  # This renders home/index.html
+
+# def result(request):
+    # return render(request, 'home/ml.html')  # This renders home/index.html
